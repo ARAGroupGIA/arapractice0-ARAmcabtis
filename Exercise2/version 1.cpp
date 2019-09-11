824d7b850a0639e12ae0338257d4915c060a5237
Marcos Cabaña Tissot <55189002+ARAmcabtis@users.noreply.github.com>
2019-09-11 18:33:40 +0200

    Version 2

--- Exercise2/version 1.cpp
+++ Exercise2/version 1.cpp
@@ -2,12 +2,14 @@
 #include <stdio.h>
 main(){
 	/* Here, you should write the source code of your program */
-	int x, y, s;
+	int x, y, z, s;
 	
 	printf("Please, type the first number");
 	scanf("%d", &x);
 	printf("Please, type the second number");
 	scanf("%d", &y);
-	s = x+y;
+	printf("Please, type the third number");
+	scanf("%d", &z);
+	s = x+y+z;
 	printf("the solution is %d", s);
 }
