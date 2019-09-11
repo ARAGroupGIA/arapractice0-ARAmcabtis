07de024f56a7d0080bf1cc1e8073a0addee5fb69
Marcos Cabaña Tissot <55189002+ARAmcabtis@users.noreply.github.com>
2019-09-11 14:35:45 +0200

    Version 1

---
+++ Exercise2/version 1.cpp
@@ -0,0 +1,13 @@
+/* Here, you should entry the required libraries */
+#include <stdio.h>
+main(){
+	/* Here, you should write the source code of your program */
+	int x, y, s;
+	
+	printf("Please, type the first number");
+	scanf("%d", &x);
+	printf("Please, type the second number");
+	scanf("%d", &y);
+	s = x+y;
+	printf("the solution is %d", s);
+}
